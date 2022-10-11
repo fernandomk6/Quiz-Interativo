d231187f3336239aebc1c1512e155cf8a7f8b37f
fernando <fernandomk6@gmail.com>
2022-10-11 20:56:44 -0300

    Concluído projeto

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
-// configurar feedback
-// configurar reiniciar quiz
-// embaralhar as perguntas
 // regra para verificar se todas as questões estão marcadas
 
 const form = document.querySelector('.quiz-form')
 const correctAnswers = ['A', 'B', 'A', 'B', 'B']
 const scoreParagraph = document.querySelector('#score')
+const scoreFeedbackElement = document.querySelector('#result-feedback')
+const restartButton = document.querySelector('#re-start-quiz') 
+const dangerScore = 40
+const warningScore = 60
+const sucessScore = 80
+const masterScore = 100
+let score = 0
 
-form.addEventListener('submit', event => {
+const dangerFeedbackOpntions = {
+  className: 'text-danger',
+  textContent: `Hmm... você errou algumas questões chaves, 
+    mas não desanime, você pode rever as aulas de etapa 1, e tentar novamente`
+}
+
+const warningFeedback = {
+  className: 'text-warning',
+  textContent: `Você acertou boa parte das questões, você foi bem`
+}
+
+const successFeedback = {
+  className: 'text-success',
+  textContent: `Parabéns! você acertou quase tudo, errou apenas uma questão. Arrazoou!`
+}
+
+const masterFeedback = {
+  className: 'text-primary',
+  textContent: `Você acertou tudo o_o' `
+}
+
+const addToScore = questionScore => score += questionScore
+const resetScore = () => score = 0
+const renderScore = score => scoreParagraph.textContent = score
+
+const renderFeedback = ({className, textContent}) => {
+  scoreFeedbackElement.textContent = textContent
+  scoreFeedbackElement.setAttribute('class', className)
+}
+
+const checkAnswer = (userAnswer, index) => {
+  const maxScore = 100
+  const questionScore = maxScore / correctAnswers.length
+  const isRightAnswer = userAnswer === correctAnswers[index]
+
+  if (isRightAnswer) {
+    addToScore(questionScore)
+  }
+}
+
+const handleFormSubmit = event => {
   event.preventDefault()
 
-  let score = 0
   const userAnswers = [
     form.inputQuestion1.value,
     form.inputQuestion2.value,
@@ -19,14 +62,33 @@ form.addEventListener('submit', event => {
     form.inputQuestion5.value
   ]
 
-  userAnswers.forEach((userAnswer, index) => {
-    if (userAnswer === correctAnswers[index]) {
-      const maxScore = 100
-      const scorePerQuestion = Math.round( maxScore / correctAnswers.length)
+  userAnswers.forEach(checkAnswer)
 
-      score += scorePerQuestion
-    }
-  })
+  if (score <= dangerScore) {
+    renderFeedback(dangerFeedbackOpntions)
+  }
 
-  scoreParagraph.textContent = score
-})
+  if (score === warningScore) {
+    renderFeedback(warningFeedback)
+  }
+
+  if (score === sucessScore) {
+    renderFeedback(successFeedback)
+  }
+
+  if (score === masterScore) {
+    renderFeedback(masterFeedback)
+  }
+
+  renderScore(score)
+  resetScore()
+}
+
+const restartQuiz = () => {
+  const inputs = form.querySelectorAll('input')
+
+  inputs.forEach(input => input.checked = false)
+}
+
+form.addEventListener('submit', handleFormSubmit)
+restartButton.addEventListener('click', restartQuiz)
